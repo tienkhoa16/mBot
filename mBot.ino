173af0ef20fe88646ddc46d4557bc99ec1258cdf
tienkhoa16 <nguyentienkhoa22@gmail.com>
2020-11-03 14:00:56 +0800

    Debugging move forward

--- mBot.ino
+++ mBot.ino
@@ -1,54 +1,50 @@
-#include <Wire.h>
 #include <MeMCore.h>
-#include <MeRGBLed.h>
 #include "Notes.h"
 
-/********** Settings **********/
-#define LEFTIR_PIN      A0
-#define RIGHTIR_PIN     A1
+/*** Configurations ***/
+#define IR_LEFT         A0
+#define IR_RIGHT        A1
 #define LDR_PIN         A6
 #define LED_PIN         7
-#define MUSIC_PIN       8
-MeDCMotor               leftWheel(M1);          // -255 to 255
-MeDCMotor               rightWheel(M2);         // 255 to -255, ie reversed
+#define BUZZER_PIN      8
+MeDCMotor               leftWheel(M1);
+MeDCMotor               rightWheel(M2);
 MeLineFollower          lineFinder(PORT_2);     
 MeUltrasonicSensor      ultraSensor(PORT_3);    
 MeRGBLed                led(LED_PIN);
 MeBuzzer                buzzer;
 
 // Movement
-#define MOTORSPEED      220
-#define TIMETURN        (61600/MOTORSPEED)      // time for 90deg turn
-#define TIMEGRID        (180000/MOTORSPEED)     // time to travel 1 grid
-#define TIMEDELAY       20                      // delay b4 recheck position
-#define DELAYGRID       (TIMEGRID / TIMEDELAY)
-#define TIMEMUL         5                       // time multiplier for IR adjustment
-#define K_DIST          (255/2)                 // max correction to mvmt
-#define D_FRONT         10                      // cm
-#define V_LEFTIR        200                     // threshold for IR sensor values
-#define V_RIGHTIR       200
+#define MOTOR_SPEED                220
+#define TIME_TURN_90_DEGREE        260
+#define TIME_GRID                  35                       // time to travel 1 grid
+#define TIME_DELAY                 20                       // delay b4 recheck position
+#define TIME_MUL                   5                        // time multiplier for IR adjustment
+#define K_DIST                     (255/2)                  // max correction to mvmt
+#define D_FRONT                    10                       // cm
+#define V_LEFTIR                   200                      // threshold for IR sensor values
+#define V_RIGHTIR                  200
 
 // Color
-#define COL_DIST        5000                    // 10000
+#define COL_DIST        5000                    // threshold for comparing colours
 #define BLA_VAL         {255, 217, 243}
 #define GRE_VAL         {116, 108, 130}
 
-#define RED_ARR         {185,35,35}             // normalised rgb vals
+#define RED_ARR         {185, 35, 35}           // normalised RGB values
 #define GRE_ARR         {45, 100, 60}
-#define YEL_ARR         {255, 175, 100}         //325,230,135
-#define PUR_ARR         {155,150,200}
-#define BLU_ARR         {175,240,240}
+#define YEL_ARR         {255, 175, 100}
+#define PUR_ARR         {155, 150, 200}
+#define BLU_ARR         {175, 240, 240}
 #define BLA_ARR         {0,0,0}
-#define NUMCOL          6                       // black, red, green, yellow, purple, blue
+#define NUM_OF_COLOURS  6                       // black, red, green, yellow, purple, blue
 
 // Calibration
-#define CALLIBRATE_SEC  3                       // delay b4 calibration
-#define COLOUR_NO       50                      // 50
-#define IR_WAIT         100                     // delay btw measurements. IMPT!
-#define RGB_WAIT        100                     // 200
-#define LDR_WAIT        10
-#define LED_MAX         255
-#define IR_MAX          1023
+#define CALLIBRATE_SEC  3                       // delay before IR and colour calibration
+#define NUM_OF_SAMPLES  50                      // number of samples for calibration
+#define IR_WAIT         100                     // delay between IR measurements
+#define RGB_WAIT        100                     // delay between each LED flash
+#define LDR_WAIT        10                      // delay between each LDR measurement
+#define LED_MAX         255                     // max value of each RGB component
 
 
 
@@ -58,17 +54,17 @@ bool busy = true;
 int irArray[2][2] = {{68,10},{81,19}}; // left-right, minmax
 int blackArray[] = BLA_VAL;
 int greyDiff[] = GRE_VAL;
-static int allColourArray[6][3] = {BLA_ARR,RED_ARR,GRE_ARR,YEL_ARR,PUR_ARR,BLU_ARR};
+static int allColourArray[6][3] = {BLA_ARR, RED_ARR, GRE_ARR, YEL_ARR, PUR_ARR, BLU_ARR};
   
 
 
 /********** Main Program **********/
 void setup() {
-  pinMode(LEFTIR_PIN, INPUT);
-  pinMode(RIGHTIR_PIN, INPUT);
+  pinMode(IR_LEFT, INPUT);
+  pinMode(IR_RIGHT, INPUT);
   pinMode(LDR_PIN, INPUT);
   pinMode(LED_PIN, OUTPUT);
-  pinMode(MUSIC_PIN, OUTPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
   Serial.begin(9600);
 
   // calibrateWB();
@@ -76,89 +72,95 @@ void setup() {
   busy = false;
 }
 
-void stopMove(const int i);
+void stopRunning(const int i);
 void loop() {
-  if (busy) return;
-
-  if (lineFinder.readSensors() != S1_IN_S2_IN) { // both sensors not in black line
-    moveForward();
-    return;
-  }
-  
-  // Waypoint detected!
-  busy = true;
-  stopMove(0);
-
-  // Color Challenge
-  int colourRes;
-  do {
-    colourRes = getColour();
-    printColour(colourRes);
-  } while (colourRes == -1);
-  if (colourRes > 0) { // is color challenge (not black)
-    colorWaypoint(colourRes);
-    busy = false;
-    return;
-  }
-
-  // Finished!
-  finishWaypoint();
+//  if (busy) return;
+//
+//  if (!isAtBlackLine()) { // both sensors not in black line
+//    moveForward();
+//    return;
+//  }
+//  
+//  // waypoint detected
+//  busy = true;
+//  stopRunning(0);
+//
+//  // color challenge
+//  int colourRes;
+//  do {
+//    colourRes = getColour();
+//    printColour(colourRes);
+//  } while (colourRes == -1);
+//
+//  if (colourRes > 0) { // is color challenge (not black)
+//    colorWaypoint(colourRes);
+//    busy = false;
+//    return;
+//  }
+//
+//  // finished
+//  finishWaypoint();
+  moveForward();
 }
 
 
-/********** Movement **********/
-void stopMove(const int i = 10) {
+/*** Movement ***/
+void stopRunning(const int i = 10) {
   rightWheel.stop();
   leftWheel.stop();
-  if (i) delay(TIMEDELAY * i);
+  if (i) delay(TIME_DELAY * i);
 }
 
 void moveForward() {
   if (ultraSensor.distanceCm() < D_FRONT) return;
   const int dx = getDist();
     
-  // Normalise to MOTORSPEED
-  int maxx = MOTORSPEED + (dx >= 0 ? dx : -dx);
-  leftWheel.run((long long)(-MOTORSPEED + dx) * MOTORSPEED / maxx);
-  rightWheel.run((long long)(MOTORSPEED + dx) * MOTORSPEED / maxx);
-    
-  delay(TIMEDELAY * TIMEMUL);
-  stopMove(0);
+  // Normalise to MOTOR_SPEED
+  int maxx = MOTOR_SPEED + (dx >= 0 ? dx : -dx);
+  leftWheel.run((long long)(-MOTOR_SPEED + dx) * MOTOR_SPEED / maxx);
+  rightWheel.run((long long)(MOTOR_SPEED + dx) * MOTOR_SPEED / maxx);
+
+  delay(TIME_DELAY * TIME_MUL);
+  stopRunning(0);
 }
 
 void forwardGrid() {
-  for (int i = 0; i < DELAYGRID; ++i) { 
+  for (int i = 0; i < TIME_GRID; i++) { 
     if (ultraSensor.distanceCm() < D_FRONT) break;
-    leftWheel.run(-MOTORSPEED);
-    rightWheel.run(MOTORSPEED);
-    delay(TIMEDELAY);
+    leftWheel.run(-MOTOR_SPEED);
+    rightWheel.run(MOTOR_SPEED);
+    delay(TIME_DELAY);
   }
-  stopMove();
+  stopRunning();
 }
 
 void turnRight() {
-  leftWheel.run(-MOTORSPEED);
-  rightWheel.run(-MOTORSPEED);
-  delay(TIMETURN);
-  stopMove();
+  leftWheel.run(-MOTOR_SPEED);
+  rightWheel.run(-MOTOR_SPEED);
+  delay(TIME_TURN_90_DEGREE);
+  stopRunning();
 }
 
 void turnLeft() {
-  leftWheel.run(MOTORSPEED);
-  rightWheel.run(MOTORSPEED);
-  delay(TIMETURN);
-  stopMove();
+  leftWheel.run(MOTOR_SPEED);
+  rightWheel.run(MOTOR_SPEED);
+  delay(TIME_TURN_90_DEGREE);
+  stopRunning();
 }
 
 void doubleRight() {
   turnRight();
+  stopRunning(15);
   forwardGrid();
+  stopRunning(15);
   turnRight();
 }
 
 void doubleLeft() {
   turnLeft();
+  stopRunning(15);
   forwardGrid();
+  stopRunning(15);
   turnLeft();
 }
 
@@ -169,26 +171,25 @@ void uTurn() {
 
 
 
-/********** Sensors **********/
+/*** Sensors ***/
 int getDist() {
   // Take raw value and threshold
-  int irVolt = analogRead(LEFTIR_PIN);
+  int irVolt = analogRead(IR_LEFT);
   if (irVolt < V_LEFTIR) // turn right
     return (irVolt - V_LEFTIR) * K_DIST / V_LEFTIR;
 
-  irVolt = analogRead(RIGHTIR_PIN);
+  irVolt = analogRead(IR_RIGHT);
   if (irVolt < V_RIGHTIR) // turn left
     return (V_RIGHTIR - irVolt) * K_DIST / V_RIGHTIR;
   
   return 0;
 }
 
-// Color Sensor: Return nearest colour
 long long square(const long long x) { return x * x; }
 int getColour() {
   // Read colours
   float colourArray[3] = {0};
-  for (int i = 0; i < 3; ++i) { // red, green, blue
+  for (int i = 0; i < 3; i++) { // red, green, blue
     led.setColor( // one-hot encoding
       ((1<<i)   &1) * LED_MAX,
       ((1<<i>>1)&1) * LED_MAX,
@@ -197,11 +198,11 @@ int getColour() {
     led.show();
     delay(RGB_WAIT);
 
-    for (int j = 0; j < COLOUR_NO; ++j) { // take avg reading
+    for (int j = 0; j < NUM_OF_SAMPLES; j++) { // take avg reading
       colourArray[i] += analogRead(LDR_PIN);
       delay(LDR_WAIT);
     }
-    colourArray[i] /= COLOUR_NO;
+    colourArray[i] /= NUM_OF_SAMPLES;
     colourArray[i] = (colourArray[i] - blackArray[i]) * 255 / greyDiff[i];
     Serial.println(colourArray[i]);
   }
@@ -210,9 +211,9 @@ int getColour() {
   // Find colour with min euclidean distance > COL_DIST
   int idx = -1;
   int min_dist = COL_DIST;
-  for (int i = 0; i < 6; ++i) {
+  for (int i = 0; i < 6; i++) {
     long long curr_dist = 0;
-    for (int j = 0; j < 3; ++j)
+    for (int j = 0; j < 3; j++)
       curr_dist += square(allColourArray[i][j] - colourArray[j]);
 
     if (min_dist > curr_dist && curr_dist > 0) {
@@ -225,65 +226,54 @@ int getColour() {
   return idx;
 }
 
+bool isAtBlackLine() {
+  return lineFinder.readSensors() == S1_IN_S2_IN;
+}
 
 
-/********** Waypoints **********/
+
+/*** Waypoints ***/
 void colorWaypoint(const int colourRes) {
-  // red : left
-  // green : right
-  // yellow : 180deg within grid
-  // purple : 2 left
-  // light blue : 2 right
   switch (colourRes) {
-    case 1: turnLeft(); break;
-    case 2: turnRight(); break;
-    case 3: uTurn(); break;
-    case 4: doubleLeft(); break;
-    case 5: doubleRight(); break;
+    case 1: turnLeft(); break;    // red
+    case 2: turnRight(); break;   // green
+    case 3: uTurn(); break;       // yellow
+    case 4: doubleLeft(); break;  // purple
+    case 5: doubleRight(); break; // light blue
   }
 }
 
 void finishWaypoint() {
   // keys and durations found in NOTES.h
   int wholenote = (60000 * 4) / tempo;
-  for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
+  for (int i = 0; i < notes * 2; i = i + 2) {
     // calculates the duration of each note
-    int divider = melody[thisNote + 1];
+    int divider = melody[i + 1];
     int noteDuration = 0;
     if (divider > 0) {
       // regular note, just proceed
       noteDuration = (wholenote) / divider;
     } else if (divider < 0) {
-      // dotted notes are represented with negative durations!!
+      // dotted notes are represented with negative durations
       noteDuration = (wholenote) / abs(divider);
       noteDuration *= 1.5; // increases the duration in half for dotted notes
     }
   
     // we only play the note for 90% of the duration, leaving 10% as a pause
-    buzzer.tone(MUSIC_PIN, melody[thisNote], noteDuration * 0.9);
+    buzzer.tone(BUZZER_PIN, melody[i], noteDuration * 0.9);
   
     // Wait for the specief duration before playing the next note.
     delay(noteDuration);
   
     // stop the waveform generation before the next note.
-    buzzer.noTone(MUSIC_PIN);
+    buzzer.noTone(BUZZER_PIN);
   }
 }
 
 
-
-/********** Calibration **********/
-// Used only for testing purposes, not for actual run.
-int norm(const int val, const int *low_mult) {
-  int tmp = 1023LL * (val - *low_mult) / *(low_mult+1);
-  if (tmp < 0) tmp = 1; // so we know it's not 0
-  else if (tmp > 1023) tmp = 1023;
-  
-  return tmp;
-}
-
+/*** Calibration ***/
 void calibrateIR() {
-  Serial.println("===== CALIBRATING IR SENSORS (SIDE) =====");
+  Serial.println("CALIBRATING IR SENSORS");
 
   // Min values
   Serial.print("COVER SENSORS. Calibrating MIN in ");
@@ -291,35 +281,30 @@ void calibrateIR() {
     Serial.print(i); Serial.print(".. "); delay(1000);
   }
   irArray[0][0] = irArray[1][0] = 0;
-  for (int i = 0; i < COLOUR_NO; ++i) {
-    irArray[0][0] += analogRead(LEFTIR_PIN);
-    irArray[1][0] += analogRead(RIGHTIR_PIN);
+  for (int i = 0; i < NUM_OF_SAMPLES; ++i) {
+    irArray[0][0] += analogRead(IR_LEFT);
+    irArray[1][0] += analogRead(IR_RIGHT);
     delay(IR_WAIT);
   }
-  irArray[0][0] /= COLOUR_NO;
-  irArray[1][0] /= COLOUR_NO;
+  irArray[0][0] /= NUM_OF_SAMPLES;
+  irArray[1][0] /= NUM_OF_SAMPLES;
   Serial.println("done.");
 
   // Max values
   Serial.print("UNCOVER SENSORS. Calibrating MAX in ");
-  for (int i = CALLIBRATE_SEC; i > 0; --i) {
+  for (int i = CALLIBRATE_SEC; i > 0; i--) {
     Serial.print(i); Serial.print(".. "); delay(1000);
   }
   irArray[0][1] = irArray[1][1] = 0;
-  for (int i = 0; i < COLOUR_NO; ++i) {
-    irArray[0][1] += analogRead(LEFTIR_PIN);
-    irArray[1][1] += analogRead(RIGHTIR_PIN);
+  for (int i = 0; i < NUM_OF_SAMPLES; ++i) {
+    irArray[0][1] += analogRead(IR_LEFT);
+    irArray[1][1] += analogRead(IR_RIGHT);
     delay(IR_WAIT);
   }
-  irArray[0][1] /= COLOUR_NO;
-  irArray[1][1] /= COLOUR_NO;
-  Serial.println("done.\n");
+  irArray[0][1] /= NUM_OF_SAMPLES;
+  irArray[1][1] /= NUM_OF_SAMPLES;
 
   // Save range
-  // Serial.println(irArray[0][0]);
-  // Serial.println(irArray[0][1]);
-  // Serial.println(irArray[1][0]);
-  // Serial.println(irArray[1][1]);
   irArray[0][1] -= irArray[0][0]; // left range
   irArray[1][1] -= irArray[1][0]; // right range
 
@@ -332,15 +317,14 @@ void calibrateIR() {
 }
 
 void calibrateWB() {
-  Serial.println("===== CALIBRATING COLOR SENSORS (TOP) =====");
   int whiteArray[3] = {0};
 
   // Max values
   Serial.print("Put WHITE sample. Calibrating MAX in ");
-  for (int i = CALLIBRATE_SEC; i > 0; --i) {
+  for (int i = CALLIBRATE_SEC; i > 0; i--) {
     Serial.print(i); Serial.print(".. "); delay(1000);
   }
-  for (int i = 0; i < 3; ++i) {
+  for (int i = 0; i < 3; i++) {
     led.setColor( // one-hot encoding
       ((1<<i)   &1) * LED_MAX,
       ((1<<i>>1)&1) * LED_MAX,
@@ -348,21 +332,21 @@ void calibrateWB() {
     ); led.show();
     delay(RGB_WAIT);
 
-    for (int j = 0; j < COLOUR_NO; ++j) {
+    for (int j = 0; j < NUM_OF_SAMPLES; j++) {
       whiteArray[i] += analogRead(LDR_PIN);
       delay(LDR_WAIT);
     }
-    whiteArray[i] /= COLOUR_NO;
+    whiteArray[i] /= NUM_OF_SAMPLES;
   }
   led.setColor(0,0,0); led.show();
   Serial.println("done.");
 
   // Min values
   Serial.print("Put BLACK sample. Calibrating MIN in ");
-  for (int i = CALLIBRATE_SEC; i > 0; --i) {
+  for (int i = CALLIBRATE_SEC; i > 0; i--) {
     Serial.print(i); Serial.print(".. "); delay(1000);
   }
-  for (int i = 0; i < 3; ++i) {
+  for (int i = 0; i < 3; i++) {
     led.setColor( // one-hot encoding
       ((1<<i)   &1) * LED_MAX,
       ((1<<i>>1)&1) * LED_MAX,
@@ -371,11 +355,11 @@ void calibrateWB() {
     delay(RGB_WAIT);
     
     blackArray[i] = 0;
-    for (int j = 0; j < COLOUR_NO; ++j) {
+    for (int j = 0; j < NUM_OF_SAMPLES; j++) {
       blackArray[i] += analogRead(LDR_PIN);
       delay(LDR_WAIT);
     }
-    blackArray[i] /= COLOUR_NO;
+    blackArray[i] /= NUM_OF_SAMPLES;
     greyDiff[i] = whiteArray[i] - blackArray[i];
   }
   led.setColor(0,0,0); led.show();
@@ -409,14 +393,13 @@ void getColourCode() {
     led.show();
     delay(RGB_WAIT);
 
-    for (int j = 0; j < COLOUR_NO; ++j) { // take avg reading
+    for (int j = 0; j < NUM_OF_SAMPLES; j++) { // take avg reading
       colourArray[i] += analogRead(LDR_PIN);
       delay(LDR_WAIT);
     }
-    colourArray[i] /= COLOUR_NO;
+    colourArray[i] /= NUM_OF_SAMPLES;
     colourArray[i] = (colourArray[i] - blackArray[i]) * 255 / greyDiff[i];
-    Serial.print(colourArray[i]);
-    Serial.print(", ");
+    Serial.print(colourArray[i]); Serial.print(", ");
   }
   led.setColor(0,0,0); led.show();  
 }
